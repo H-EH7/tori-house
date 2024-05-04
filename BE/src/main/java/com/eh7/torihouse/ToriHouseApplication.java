ea8a3185b8173b4ae274ed33fcb30b7cbf9d38c4
이현택 <fly3767@gmail.com>
2024-05-05 00:41:58 +0900

    init

---
+++ BE/src/main/java/com/eh7/torihouse/ToriHouseApplication.java
@@ -0,0 +1,13 @@
+package com.eh7.torihouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToriHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToriHouseApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/eh7/torihouse/ToriHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eh7.torihouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToriHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
